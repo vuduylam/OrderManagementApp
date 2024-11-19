10082daa1afc2ebcd27f53fd46246323bf479dd0
vuduylam <vuduylam250302@gmail.com>
2024-11-19 14:55:04 +0700

    create DTO

---
+++ DTOs/CategoryDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OrderManagementApp.DTOs
+{
+    public class CategoryDTO
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string Unit { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
---
+++ DTOs/ProductDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace OrderManagementApp.DTOs
+{
+    public class ProductDTO
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } =string.Empty;
+        public int CategoryId { get; set; }
+        public string Unit { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
